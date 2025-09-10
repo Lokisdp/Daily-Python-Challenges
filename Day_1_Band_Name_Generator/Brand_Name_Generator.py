23495dfdd5b10bd9a77dbfd478c122c7de12dd6a
Lokesh <sodanapallilokeshreddy@gmail.com>
2025-09-10 15:20:14 +0200

    Added Day 1 challenge

---
+++ Day_1_Band_Name_Generator/Brand_Name_Generator.py
@@ -0,0 +1,4 @@
+print("Welcome to my Program.")
+City = input("which city did you grow up?\n")
+Pet_name = input("What is your pet name?\n")
+print("This is your band name: " + City +" " + Pet_name)
